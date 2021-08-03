3fbe2d0cfaf66c3af7858934526586bbd27842f7
Ian Smith <iansmith@honeycomb.io>
2021-08-03 14:22:55 -0700

    try skipping the 2nd layer and using pip instead

--- build.sh
+++ build.sh
@@ -14,11 +14,11 @@ confluent_image="confluentinc/cp-$service:$version"
 # _Always_ amd64 even if builder is on arm64
 docker pull --platform linux/amd64 "$confluent_image"
 docker save "$confluent_image" | tar -x
-# We do not want the first layer; we want layers >=1 (if zero-indexed), so
-# tail -n +2 (1-indexed)
+# We do not want the first two layers; we want layers >=2 (if zero-indexed), so
+# tail -n +3 (1-indexed)
 layers=$(cat manifest.json \
     | jq -r '.[0].Layers[]' \
-    | tail -n +2)
+    | tail -n +3)
 mkdir newtar; cd newtar
 for l in $layers; do
     tar -xf ../$l
@@ -26,6 +26,7 @@ done
 cd -
 
 cp ../cmd .
+cp ../get-pip.py .
 tar -cvzf newtar.tar.gz newtar --transform='s/^newtar//g'
 rm -rf newtar
 
@@ -36,6 +37,14 @@ rm -r repositories
 
 cat > Dockerfile <<- EOF
 FROM openjdk:18-jdk-buster
+
+# This is in the 2nd layer, but is not cross-platform, so we install it here
+COPY get-pip.py .
+
+# I don't know why confluent-docker-utils fails to install with pip>9.0.3, but
+# it does: https://stackoverflow.com/a/51153611
+RUN python get-pip.py "pip==9.0.3" \
+  && pip install --no-cache-dir git+https://github.com/confluentinc/confluent-docker-utils@v0.0.20
 
 COPY newtar.tar.gz /newtar.tar.gz
 COPY cmd /etc/confluent/docker/run
